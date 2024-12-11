734a3f4dde2056a55a442f3965121a3fb2555a06
grover.dipin007 <grover.dipin007@gmail.com>
2024-12-11 11:51:47 +0000

    added datypes and variables naming rules

---
+++ datatype_demo.js
@@ -0,0 +1,8 @@
+let product_price = 150000;
+
+let product_discount = 15;
+
+let product_name = "Apple Iphone 11 Pro Max (512 GB , Blue)";
+
+let is_avaialable = true;
+
---
+++ variable_name.js
@@ -0,0 +1,14 @@
+// rule 1 variable name can have english alphabets (a-z) or (A-Z) and numbers and underscores and dollars only
+
+// rule 2 variable name cannot start with a number / digit.
+
+// rule 3 variable names are case-sensitive.
+
+// rule 4 you cannot use a keyword in variable name
+
+let first_name = "Dipin";
+
+console.log(first_name);
+
+let phone1 = 99999999;
+
