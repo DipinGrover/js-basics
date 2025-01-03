f7a9007a793ad176154628cffba1182b496fa029
grover.dipin007 <grover.dipin007@gmail.com>
2025-01-03 17:02:26 +0000

    added new demo

--- datatype_demo.js
+++ datatype_demo.js
@@ -5,3 +5,20 @@ let product_discount = 15;
 let product_name = "Apple Iphone 15 Pro Max (256 GB, Black)";
 
 let is_avialable = true;
+
+let minuszero = -0;
+
+console.log(minuszero);
+
+let x;
+
+console.log(x);
+
+let y = null;
+
+console.log(y);
+
+
+let bignumber = 73717398471298371283921738921738921738129371298312738912789327389237981873284971289n;
+
+console.log(bignumber);
---
+++ object_demo.js
@@ -0,0 +1,19 @@
+let product_details = {
+    product_name : "Apple Iphone 14 Pro Max (512 GB , Black)",
+    price : 125000,
+    'company' : "Apple",
+    discount : 20 ,
+    is_avialable : false
+};
+
+
+let social_handles = {
+    'facebook' : 4000,
+    instagram : 13000,
+    "linkedin" : 2000,
+    twitter : 3000,
+    instagram : 24000  // instagram value will be updated
+};
+
+console.log(product_details);
+console.log(social_handles);
