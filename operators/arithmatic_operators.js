51fc8137b35c41fba40bf89ed20e5646c1799cda
grover.dipin007 <grover.dipin007@gmail.com>
2025-01-03 17:47:44 +0000

    added arithmatic operators

---
+++ operators/arithmatic_operators.js
@@ -0,0 +1,17 @@
+let x = 2;
+
+let y = 3;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y); // gives the remainder when x is divided by y
+
+console.log(17%3);
+
+console.log(x ** y);
